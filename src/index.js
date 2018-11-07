df4f5c5687d48d9e2d9252497ba40874591d5fc6
Ed Putans <edputans@gmail.com>
2018-11-07 11:59:34 +0000

    figured out the <tr>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
+const dogForm = document.querySelector('#dog-form')
+const tableBody = document.querySelector('#table-body')
 
-})
+const retrieveDogs = () =>
+    fetch('http://localhost:3000/dogs').then(resp=>resp.json())
+
+
+const addDoggo = dogObject =>{
+   const dogEl = document.createElement('tr')
+    dogEl.innerHTML=`<td>${dogObject.name}</td>
+    <td>${dogObject.breed}</td>
+    <td>${dogObject.sex}</td>
+    <td><button>Edit</button></td>`
+
+    tableBody.appendChild(dogEl)
+}
+
+const shoveDogsToPage = dogs => dogs.forEach(dog => addDoggo(dog))
+
+retrieveDogs().then(dogs => shoveDogsToPage(dogs))
+
+
